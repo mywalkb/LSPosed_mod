92a04e3698ff5756318d1bf5abe8f3665d17f751
mywalk <66966897+mywalkb@users.noreply.github.com>
2024-03-03 13:17:47 +0100

    [core] fix #34
    Support for stripped library as libart.
    It require which a new section .gnu_debugdata, compressed
    with xz library, exist in elf header of the library.
    After in memory decompression, new elf header is parsed, so can find
    section .symtab

--- core/src/main/jni/include/elf_util.h
+++ core/src/main/jni/include/elf_util.h
@@ -101,12 +101,17 @@ namespace SandHook {
 
         void MayInitLinearMap() const;
 
+        void parse(ElfW(Ehdr) *header);
+
+        bool xzdecompress();
+
         std::string elf;
         void *base = nullptr;
         char *buffer = nullptr;
         off_t size = 0;
         off_t bias = -4396;
         ElfW(Ehdr) *header = nullptr;
+        ElfW(Ehdr) *header_debugdata = nullptr;
         ElfW(Shdr) *section_header = nullptr;
         ElfW(Shdr) *symtab = nullptr;
         ElfW(Shdr) *strtab = nullptr;
@@ -120,6 +125,9 @@ namespace SandHook {
         ElfW(Off) symtab_offset = 0;
         ElfW(Off) dynsym_offset = 0;
         ElfW(Off) symtab_size = 0;
+        ElfW(Off) debugdata_offset = 0;
+        ElfW(Off) debugdata_size = 0;
+        std::string elf_debugdata;
 
         uint32_t nbucket_{};
         uint32_t *bucket_ = nullptr;
--- core/src/main/jni/src/elf_util.cpp
+++ core/src/main/jni/src/elf_util.cpp
@@ -26,6 +26,7 @@
 #include <sys/stat.h>
 #include "logging.h"
 #include "elf_util.h"
+#include "xz/xz.h"
 
 using namespace SandHook;
 
@@ -56,13 +57,23 @@ ElfImg::ElfImg(std::string_view base_name) : elf(base_name) {
     header = reinterpret_cast<decltype(header)>(mmap(nullptr, size, PROT_READ, MAP_SHARED, fd, 0));
 
     close(fd);
+    parse(header);
+    if (debugdata_offset != 0 && debugdata_size != 0) {
+        if (xzdecompress()) {
+            header_debugdata = reinterpret_cast<ElfW(Ehdr) *>(elf_debugdata.data());
+            parse(header_debugdata);
+        }
+    }
+}
 
-    section_header = offsetOf<decltype(section_header)>(header, header->e_shoff);
+void ElfImg::parse(ElfW(Ehdr) *hdr)
+{
+    section_header = offsetOf<decltype(section_header)>(hdr, hdr->e_shoff);
 
     auto shoff = reinterpret_cast<uintptr_t>(section_header);
-    char *section_str = offsetOf<char *>(header, section_header[header->e_shstrndx].sh_offset);
+    char *section_str = offsetOf<char *>(hdr, section_header[hdr->e_shstrndx].sh_offset);
 
-    for (int i = 0; i < header->e_shnum; i++, shoff += header->e_shentsize) {
+    for (int i = 0; i < hdr->e_shnum; i++, shoff += hdr->e_shentsize) {
         auto *section_h = (ElfW(Shdr) *) shoff;
         char *sname = section_h->sh_name + section_str;
         auto entsize = section_h->sh_entsize;
@@ -71,7 +82,8 @@ ElfImg::ElfImg(std::string_view base_name) : elf(base_name) {
                 if (bias == -4396) {
                     dynsym = section_h;
                     dynsym_offset = section_h->sh_offset;
-                    dynsym_start = offsetOf<decltype(dynsym_start)>(header, dynsym_offset);
+                    dynsym_start = offsetOf<decltype(dynsym_start)>(hdr, dynsym_offset);
+                    LOGD("dynsym header {:#x} size {}", section_h->sh_offset, section_h->sh_size);
                 }
                 break;
             }
@@ -81,7 +93,8 @@ ElfImg::ElfImg(std::string_view base_name) : elf(base_name) {
                     symtab_offset = section_h->sh_offset;
                     symtab_size = section_h->sh_size;
                     symtab_count = symtab_size / entsize;
-                    symtab_start = offsetOf<decltype(symtab_start)>(header, symtab_offset);
+                    symtab_start = offsetOf<decltype(symtab_start)>(hdr, symtab_offset);
+                    LOGD("symtab header {:#x} size {} found in {}", section_h->sh_offset, section_h->sh_size, debugdata_offset != 0 ? "gnu_debugdata" : "orgin elf");
                 }
                 break;
             }
@@ -89,7 +102,8 @@ ElfImg::ElfImg(std::string_view base_name) : elf(base_name) {
                 if (bias == -4396) {
                     strtab = section_h;
                     symstr_offset = section_h->sh_offset;
-                    strtab_start = offsetOf<decltype(strtab_start)>(header, symstr_offset);
+                    strtab_start = offsetOf<decltype(strtab_start)>(hdr, symstr_offset);
+                    LOGD("strtab header {:#x} size {}", section_h->sh_offset, section_h->sh_size);
                 }
                 if (strcmp(sname, ".strtab") == 0) {
                     symstr_offset_for_symtab = section_h->sh_offset;
@@ -97,6 +111,11 @@ ElfImg::ElfImg(std::string_view base_name) : elf(base_name) {
                 break;
             }
             case SHT_PROGBITS: {
+                if (strcmp(sname, ".gnu_debugdata") == 0) {
+                    debugdata_offset = section_h->sh_offset;
+                    debugdata_size = section_h->sh_size;
+                    LOGD("gnu_debugdata header {:#x} size {}", section_h->sh_offset, section_h->sh_size);
+                }
                 if (strtab == nullptr || dynsym == nullptr) break;
                 if (bias == -4396) {
                     bias = (off_t) section_h->sh_addr - (off_t) section_h->sh_offset;
@@ -104,14 +123,14 @@ ElfImg::ElfImg(std::string_view base_name) : elf(base_name) {
                 break;
             }
             case SHT_HASH: {
-                auto *d_un = offsetOf<ElfW(Word)>(header, section_h->sh_offset);
+                auto *d_un = offsetOf<ElfW(Word)>(hdr, section_h->sh_offset);
                 nbucket_ = d_un[0];
                 bucket_ = d_un + 2;
                 chain_ = bucket_ + nbucket_;
                 break;
             }
             case SHT_GNU_HASH: {
-                auto *d_buf = reinterpret_cast<ElfW(Word) *>(((size_t) header) +
+                auto *d_buf = reinterpret_cast<ElfW(Word) *>(((size_t) hdr) +
                                                              section_h->sh_offset);
                 gnu_nbucket_ = d_buf[0];
                 gnu_symndx_ = d_buf[1];
@@ -125,6 +144,110 @@ ElfImg::ElfImg(std::string_view base_name) : elf(base_name) {
             }
         }
     }
+}
+
+bool ElfImg::xzdecompress() {
+    struct xz_buf str_xz_buf;
+    struct xz_dec *str_xz_dec;
+    enum xz_ret ret = XZ_OK;
+    bool bError = true;
+
+    #define BUFSIZE 1024*1024
+
+    xz_crc32_init();
+#ifdef XZ_USE_CRC64
+    xz_crc64_init();
+#endif
+    str_xz_dec = xz_dec_init(XZ_DYNALLOC, 1 << 26);
+    if (str_xz_dec == NULL) {
+        LOGE("xz_dec_init memory allocation failed");
+        return false;
+    }
+
+    uint8_t *sBuffOut = (uint8_t *)malloc(BUFSIZE);
+    if (sBuffOut == NULL) {
+        LOGE("allocation for debugdata_header failed");
+        return false;
+    }
+
+    int iSzOut = BUFSIZE;
+
+    str_xz_buf.in = ((uint8_t *)header)+debugdata_offset;
+    str_xz_buf.in_pos = 0;
+    str_xz_buf.in_size = debugdata_size;
+    str_xz_buf.out = sBuffOut;
+    str_xz_buf.out_pos = 0;
+    str_xz_buf.out_size = BUFSIZE;
+
+    uint8_t iSkip = 0;
+
+    while (true) {
+        ret = xz_dec_run(str_xz_dec, &str_xz_buf);
+
+        if (str_xz_buf.out_pos == BUFSIZE) {
+            str_xz_buf.out_pos = 0;
+            iSkip++;
+        } else {
+            iSzOut -= (BUFSIZE - str_xz_buf.out_pos);
+        }
+
+        if (ret == XZ_OK) {
+            iSzOut += BUFSIZE;
+            sBuffOut = (uint8_t *)realloc(sBuffOut, iSzOut);
+            str_xz_buf.out = sBuffOut+(iSkip*BUFSIZE);
+            continue;
+        }
+
+#ifdef XZ_DEC_ANY_CHECK
+        if (ret == XZ_UNSUPPORTED_CHECK) {
+            LOGW("Unsupported check; not verifying file integrity");
+            continue;
+        }
+#endif
+        break;
+    } // end while true
+
+    switch (ret) {
+        case XZ_STREAM_END:
+            bError = false;
+            break;
+
+        case XZ_MEM_ERROR:
+            LOGE("Memory allocation failed");
+            break;
+
+        case XZ_MEMLIMIT_ERROR:
+            LOGE("Memory usage limit reached");
+            break;
+
+        case XZ_FORMAT_ERROR:
+            LOGE("Not a .xz file");
+            break;
+
+        case XZ_OPTIONS_ERROR:
+            LOGE("Unsupported options in the .xz headers");
+            break;
+
+        case XZ_DATA_ERROR:
+        case XZ_BUF_ERROR:
+            LOGE("File is corrupt");
+            break;
+
+        default:
+            LOGE("xz_dec_run return a wrong value!");
+            break;
+    }
+    xz_dec_end(str_xz_dec);
+    if (bError) {
+        return false;
+    }
+    if (sBuffOut[0] != 0x7F && sBuffOut[1] != 0x45 && sBuffOut[2] != 0x4C && sBuffOut[3] != 0x46) {
+        LOGE("not ELF header in gnu_debugdata");
+        return false;
+    }
+    elf_debugdata = std::string((char *)sBuffOut, iSzOut);
+    free(sBuffOut);
+    return true;
 }
 
 ElfW(Addr) ElfImg::ElfLookup(std::string_view name, uint32_t hash) const {
@@ -169,9 +292,10 @@ ElfW(Addr) ElfImg::GnuLookup(std::string_view name, uint32_t hash) const {
 void ElfImg::MayInitLinearMap() const {
     if (symtabs_.empty()) {
         if (symtab_start != nullptr && symstr_offset_for_symtab != 0) {
+            auto hdr = header_debugdata != nullptr ? header_debugdata : header;
             for (ElfW(Off) i = 0; i < symtab_count; i++) {
                 unsigned int st_type = ELF_ST_TYPE(symtab_start[i].st_info);
-                const char *st_name = offsetOf<const char *>(header, symstr_offset_for_symtab +
+                const char *st_name = offsetOf<const char *>(hdr, symstr_offset_for_symtab +
                                                                      symtab_start[i].st_name);
                 if ((st_type == STT_FUNC || st_type == STT_OBJECT) && symtab_start[i].st_size) {
                     symtabs_.emplace(st_name, &symtab_start[i]);
