824c70ada75ce1d5efadd26ba35db83727256014
mywalk <66966897+mywalkb@users.noreply.github.com>
2024-04-24 18:35:56 +0200

    Update java to JDK 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,8 +72,8 @@ val androidMinSdkVersion by extra(27)
 val androidBuildToolsVersion by extra("34.0.0")
 val androidCompileSdkVersion by extra(34)
 val androidCompileNdkVersion by extra("26.2.11394342")
-val androidSourceCompatibility by extra(JavaVersion.VERSION_17)
-val androidTargetCompatibility by extra(JavaVersion.VERSION_17)
+val androidSourceCompatibility by extra(JavaVersion.VERSION_21)
+val androidTargetCompatibility by extra(JavaVersion.VERSION_21)
 
 tasks.register("Delete", Delete::class) {
     delete(rootProject.layout.buildDirectory)
